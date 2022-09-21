e8a57958d98abb8c2fff583b4483d894b672a6d6
Kyle <kylevance801@gmail.com>
2022-09-20 21:53:10 -0600

    End of day 2 work

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ module.exports={
     workout(muscleGroup, name){
       return axios.get('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises', {
         params: {
-          // muscle: muscleGroup,
+        muscle: muscleGroup,
         name:name},
         headers: {
           'X-RapidAPI-Key': '7f9675d051msh33d133dcf3f8fa1p1d63bejsn093e8ff0a810',
