7207a6b450e75777fdf5a5382e4ab2a59124d4bd
James Jabar <jabar.james.jj@gmail.com>
2022-09-27 16:11:28 -0400

    updated files

---
+++ public/assets/register.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+var Register = require('../assets/register.html');
+
+
+router.post('/', (req, res, next) => {
+
+  
+
+   
+   var name = req.body.name;
+    var email = req.body.name;
+    var password = req.body.password;
+    
+   
+    errors = req.validationErrors();
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        var userData = new Register({
+            email: email,
+            name: name,
+            password: password
+        });
+        Register.createUser(userData, function (err, register) {
+            console.log(err, register);
+        });
+       
+        req.flash('success', 'User Added');
+        res.redirect('/Login');
+    }
+
+    
+
+});
+
+
+
+const redirectTodashboard = (req, res, next) => {
+    if(!req.session.passport) {
+        next();
+    } else {
+        res.redirect('public/assets/login.js');
+    }
+  }
+  
+router.get('/public/assets/login.js', redirectTodashboard, function (req, res, next) {
+    res.render('login.js', {});
+});
+
+
+module.exports = router;
